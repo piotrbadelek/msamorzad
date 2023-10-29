e137583a108ea9fc3d7e4bd30e3190bccf5de345
ProgramistaZpolski <piotrbadelek@protonmail.com>
2023-10-29 21:07:07 +0100

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+@extends("layouts.app")
+
+@section("title", "Samorząd II LO")
+
+@section("content")
+    <div class="card">
+        <img src="/img/taxes.png" alt="Składki">
+        <p class="card_content">Składki</p>
+    </div>
+    <div class="card">
+        <img src="/img/taxes.png" alt="Składki">
+        <p class="card_content">Radiowęzeł</p>
+    </div>
+    <div class="card">
+        <img src="/img/taxes.png" alt="Składki">
+        <p class="card_content">Konkursy</p>
+    </div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=yes, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield("title")</title>
+    <link rel="stylesheet" href="/style.css">
+</head>
+<body>
+    <header>
+        <b>Samorząd II LO (2 i-m)</b>
+    </header>
+    <main>
+        @yield("content")
+    </main>
+</body>
+</html>
