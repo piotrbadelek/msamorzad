68dee164def31132c766f183c496487140e16901
ProgramistaZpolski <piotrbadelek@protonmail.com>
2023-12-09 10:11:39 +0100

    Redesign payment details

--- resources/views/payment/show.blade.php
+++ resources/views/payment/show.blade.php
@@ -8,14 +8,14 @@
 		Suma: {{ count($paid) * $payment->amount + count($not_paid) * $payment->amount }} zł.</p>
 	<b>Zapłaciło:</b><br>
 	<span>Łącznie: {{ count($paid) * $payment->amount }} zł</span>
-	<ul>
+	<ul class="payment-students">
 		@foreach($paid as $user)
 			<li><a href="/skladki/{{ $payment->id }}/{{ $user }}">{{ \App\Models\User::where("id", $user)->first()->name }}</a></li>
 		@endforeach
 	</ul>
 	<b>Nie zapłaciło:</b><br>
 	<span>Łącznie: {{ count($not_paid) * $payment->amount }} zł</span>
-	<ul>
+	<ul class="payment-students">
 		@foreach($not_paid as $user)
 			<li><a href="/skladki/{{ $payment->id }}/{{ $user->id }}">{{ $user->name }}</a></li>
 		@endforeach
