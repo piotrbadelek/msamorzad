4687d06c371856bd1eeb9b402e01dd2dfc574614
ProgramistaZpolski <piotrbadelek@protonmail.com>
2023-12-17 17:24:57 +0100

    Add maintenance mode view

---
+++ resources/views/maintenance.blade.php
@@ -0,0 +1,8 @@
+@extends("layouts.app")
+
+@section("title", "Samorząd II LO")
+
+@section("content")
+	<h1>503 Service Unavailable</h1>
+	<h2>Aplikacja jest w trakcie aktualizacji. Prosimy o cierpliwość.</h2>
+@endsection
