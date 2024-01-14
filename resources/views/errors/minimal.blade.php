17bf4ba44d563923b052764873b7c1ab477988f0
ProgramistaZpolski <piotrbadelek@protonmail.com>
2024-01-14 11:28:30 +0100

    Move errors CSS to errors.min.css for CSP

---
+++ resources/views/errors/minimal.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title')</title>
+
+		<link rel="stylesheet" href="/errors.min.css">
+    </head>
+    <body class="antialiased">
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+            <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
+                <div class="flex items-center pt-8 sm:justify-start sm:pt-0">
+                    <div class="px-4 text-lg text-gray-500 border-r border-gray-400 tracking-wider">
+                        @yield('code')
+                    </div>
+
+                    <div class="ml-4 text-lg text-gray-500 uppercase tracking-wider">
+                        @yield('message')
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
