e47acd84f0554b55b4fabc3a3202e243bc3a45ef
ProgramistaZpolski <piotrbadelek@protonmail.com>
2024-01-27 08:14:41 +0100

    Add aria-hidden attributes to index images

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,39 +11,39 @@
 	@unless($user->isTeacher && $user->notManagingAClass)
 		<a href="/skladki" class="card-container">
 			<div class="card">
-				<img src="/img/money.webp" alt="Składki">
+				<img src="/img/money.webp" alt="Składki" aria-hidden="true">
 				<p class="card_content">Składki</p>
 			</div>
 		</a>
 	@endunless
 	<a href="/contests" class="card-container">
 		<div class="card">
-			<img src="/img/contests.webp" alt="Konkursy">
+			<img src="/img/contests.webp" alt="Konkursy" aria-hidden="true">
 			<p class="card_content">Konkursy</p>
 		</div>
 	</a>
 	<a href="/announcements" class="card-container">
 		<div class="card">
-			<img src="/img/ogloszenia.webp" alt="Ogłoszenia">
+			<img src="/img/ogloszenia.webp" alt="Ogłoszenia" aria-hidden="true">
 			<p class="card_content">Ogłoszenia</p>
 		</div>
 	</a>
 	<a href="/messages" class="card-container">
 		<div class="card">
-			<img src="/img/contact.webp" alt="Kontakt">
+			<img src="/img/contact.webp" alt="Kontakt" aria-hidden="true">
 			<p class="card_content">Kontakt</p>
 		</div>
 	</a>
 	<a href="/about" class="card-container">
 		<div class="card">
-			<img src="/img/about.webp" alt="O aplikacji">
+			<img src="/img/about.webp" alt="O aplikacji" aria-hidden="true">
 			<p class="card_content">O aplikacji</p>
 		</div>
 	</a>
 	@if($user->isAdministrator)
 		<a href="/admin" class="card-container">
 			<div class="card">
-				<img src="/img/admin.webp" alt="Administracja">
+				<img src="/img/admin.webp" alt="Administracja" aria-hidden="true">
 				<p class="card_content">Administracja</p>
 			</div>
 		</a>
