780d31dddae6237afae7cde89be673da761785dd
ProgramistaZpolski <piotrbadelek@protonmail.com>
2024-02-10 18:22:49 +0100

    Change version of CSS file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,12 +1,12 @@
 <!doctype html>
 <html lang="pl">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=yes, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield("title")</title>
-    <link rel="stylesheet" href="/style.css?v=4">
+	<meta charset="UTF-8">
+	<meta name="viewport"
+		  content="width=device-width, user-scalable=yes, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>@yield("title")</title>
+	<link rel="stylesheet" href="/style.css?v=5">
 	<link rel="manifest" href="/manifest.json">
 	<script
 		src="https://js.sentry-cdn.com/aabbc2c94ac8133dcdb4610c04d3395d.min.js"
@@ -22,34 +22,34 @@
 	<meta name="description" content="Aplikacja samorządu uczniowskiego dla II LO w Tomaszowie Mazowieckim">
 </head>
 <body>
-    <header>
-		<a href="/"><b>mSamorząd</b></a>
-    </header>
-    <main>
-        @yield("content")
-    </main>
-	<dialog id="notificationDialogue">
-		<header>Włącz powiadomienia</header>
-		<p>Włącz powiadomienia, aby otrzymywać:</p>
-		<ul>
-			<li>Przypomenienia o składkach</li>
-			<li>Informacje o nowych składkach</li>
-			<li>Ogłoszenia samorządu uczniowskiego</li>
-			<li>Ogłoszenia konkursów</li>
-			<li>Odpowiedzi na zadane pytania</li>
-		</ul>
-		<button id="enableNotifications">Włącz</button>
-		<button class="not-primary" id="disableNotifications">Nie teraz</button>
-	</dialog>
+<header>
+	<a href="/"><b>mSamorząd</b></a>
+</header>
+<main>
+	@yield("content")
+</main>
+<dialog id="notificationDialogue">
+	<header>Włącz powiadomienia</header>
+	<p>Włącz powiadomienia, aby otrzymywać:</p>
+	<ul>
+		<li>Przypomenienia o składkach</li>
+		<li>Informacje o nowych składkach</li>
+		<li>Ogłoszenia samorządu uczniowskiego</li>
+		<li>Ogłoszenia konkursów</li>
+		<li>Odpowiedzi na zadane pytania</li>
+	</ul>
+	<button id="enableNotifications">Włącz</button>
+	<button class="not-primary" id="disableNotifications">Nie teraz</button>
+</dialog>
 
-	<dialog id="installDialogue">
-		<header>Zainstaluj aplikację</header>
-		<p>Zainstaluj aplikację, aby dodać ją do ekranu domowego</p>
-		<button id="installButton">Zainstaluj</button>
-		<button class="not-primary" id="dontInstallApp">Nie teraz</button>
-	</dialog>
-	<script src="/js/script.js?v=4" defer></script>
-	<script src="/sw.js" defer></script>
-	@yield("scripts")
+<dialog id="installDialogue">
+	<header>Zainstaluj aplikację</header>
+	<p>Zainstaluj aplikację, aby dodać ją do ekranu domowego</p>
+	<button id="installButton">Zainstaluj</button>
+	<button class="not-primary" id="dontInstallApp">Nie teraz</button>
+</dialog>
+<script src="/js/script.js?v=4" defer></script>
+<script src="/sw.js" defer></script>
+@yield("scripts")
 </body>
 </html>
