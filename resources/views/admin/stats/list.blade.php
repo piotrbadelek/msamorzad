597a93062495683828b911ad25a2c11079116bc1
ProgramistaZpolski <piotrbadelek@protonmail.com>
2024-03-07 19:13:44 +0100

    Remove NPM

--- resources/views/admin/stats/list.blade.php
+++ resources/views/admin/stats/list.blade.php
@@ -6,6 +6,6 @@
 	<h1>Administracja - statystyki</h1>
 	<x-start-card name="Aktywni użytkownicy" imageFile="users" url="admin/stats/active-users"/>
 	<x-start-card name="Laravel Pulse" imageFile="statistics" url="pulse"/>
-	<x-start-card name="Wyjdź" imageFile="backwards" url=""/>
+	<x-start-card name="Wyjdź" imageFile="backwards" url="admin"/>
 	<footer class="appver">mSamorząd {{ config("app.version") }} (on PHP {{ phpversion() }})</footer>
 @endsection
